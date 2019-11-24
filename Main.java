721aa7970bd1ee22c468766fd52c05648575c6ee
Elliott Wall <walle@csp.edu>
2019-11-24 14:25:02 -0600

    trying to figure out why survivors aren't being removed correctly

--- Main.java
+++ Main.java
@@ -16,7 +16,7 @@ public class Main {
         ArrayList<Character>survivors = new ArrayList<>();
         
         // add random number of tanks to zombie arraylist
-        for (int i = 0; i < rand.nextInt(15); i++ ){
+        for (int i = 0; i < rand.nextInt(55); i++ ){
             zombies.add(new TankZombie());
             tankCount++;
         }
@@ -45,6 +45,12 @@ public class Main {
             childCount++;
         }
 
+        System.out.println("tank count" + tankCount);
+        System.out.println("common count" + commonCount);
+        System.out.println("soldier count" + soldierCount);
+        System.out.println("teacher count " + teacherCount);
+        System.out.println("child count" + childCount);
+
         // shuffle arraylist orders
         Collections.shuffle(zombies);
         Collections.shuffle(survivors);
@@ -52,19 +58,31 @@ public class Main {
         System.out.println("\n We have " + survivors.size() + " survivors trying to make it to safety.\n");
         System.out.println("But there are " + zombies.size() + " zombies waiting for them. \n");
 
-        for (Character survivor : survivors) {
-            while (survivor.isAlive()) {
-                for (Character zombie : zombies) {
-                    while (zombie.isAlive() && survivor.isAlive()) {
-                        System.out.println(zombie.name);
-                        System.out.println(zombie.health);
-                        System.out.println(survivor.name);
-                        System.out.println(survivor.health);
-                        zombie.health -= survivor.attack;
-                        survivor.health -= zombie.attack;
-                    }
+        for (int i = 0; i < survivors.size(); i++) {
+            Character survivor = survivors.get(i);
+                for (int i1 = 0; i1 < zombies.size(); i1++) {
+                    Character zombie = zombies.get(i1);
+                        while (zombie.isAlive() && survivor.isAlive()) {
+
+                            // for testing only
+                            /* System.out.println(zombie.name);
+                            System.out.println(zombie.health);
+                            System.out.println(survivor.name);
+                            System.out.println(survivor.health); */
+
+                            zombie.health -= survivor.attack;
+                            survivor.health -= zombie.attack;
+                        }
+                    zombies.remove(zombie);
+                    System.out.println(zombies.size() + " zombies remain");
                 }
-            }
+            survivors.remove(survivor);
+            System.out.println(survivors.size() + " survivors remain");
+        }
+        if (zombies.size() > survivors.size()) {
+            System.out.println("There were no survivors and " + zombies.size() + " zombies remained.");
+        } else {
+            System.out.println("There were " + survivors.size() + " survivors that made it!");
         }
     }
 }
