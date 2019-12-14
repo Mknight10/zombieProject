5199f9752a782456848b14b9712e4c3d53ab5637
Mknight10 <56999427+Mknight10@users.noreply.github.com>
2019-12-14 01:08:54 -0500

    Update Handgun.java

--- Handgun.java
+++ Handgun.java
@@ -3,7 +3,7 @@ package project;
 public class Handgun extends Weapon {
   public Handgun() {
         String name = "Handgun";
-        Int damage = 5;
+        int damage = 5;
         double accuarcy = 0.75;
     }
 }
