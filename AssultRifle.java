0003e0aed238a8201a0b9c999b9430ffa2b22930
Mknight10 <56999427+Mknight10@users.noreply.github.com>
2019-12-13 23:34:43 -0500

    Create AssultRifle.java

---
+++ AssultRifle.java
@@ -0,0 +1,9 @@
+package project;
+
+public class AssultRifle extends Weapon {
+  public AssultRifle() {
+        name = "Assult Rifle";
+        damage = 12;
+        double accuarcy = 0.85;
+    }
+}
